11aab950a917f4bb53afa363b43f99b772054ffc
cevik <suleymancevik64@gmail.com>
2023-03-07 21:05:44 +0300

    cevik

--- src/test/java/runners/TestRun.java
+++ src/test/java/runners/TestRun.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         features = {"src/test/resources/features"},
         glue = {"stepDefinitions"},
-        tags = "@Test23",
+        tags = "@Test23 or @Test24",
         dryRun = false
 )
 public class TestRun {
